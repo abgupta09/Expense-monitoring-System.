08e097d71bf47b5c10c0fcdcd7ee9c0cb6a774d0
Abhishek Gupta <abhishekgupta@Abhisheks-MacBook-Pro.local>
2023-11-28 12:34:50 -0500

    removed global header an nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import GlobalHeader from './components/GlobalHeader';
-import Footer from './components/Footer';
 import PersonalExpenseManager from './components/PersonalExpenseManager';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
@@ -10,14 +8,14 @@ import GroupExpenseManager from './components/GroupExpenseManager';
 function App() {
     return (
         <Router>
-            <GlobalHeader />
+
             <Routes>
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/personal" element={<PersonalExpenseManager />} />
                 <Route path="/groups" element={<GroupExpenseManager />} />
             </Routes>
-            <Footer />
+
         </Router>
     );
 }
