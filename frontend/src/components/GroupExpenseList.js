1ca494a67311e251f906e025dbc27c00c7d19303
Abhishek Gupta <abhishekgupta@Abhisheks-MacBook-Pro.local>
2023-12-10 15:38:11 -0500

    updated group and api

--- frontend/src/components/GroupExpenseList.js
+++ frontend/src/components/GroupExpenseList.js
@@ -10,7 +10,11 @@ function GroupExpenseList({ groupExpenses, startEditGroupExpense, deleteGroupExp
         let formattedShares = '';
         if (splitDetails && splitDetails.shares) {
             formattedShares = Object.entries(splitDetails.shares)
-                .map(([username, share]) => `${username}: ${share}%`)
+                .map(([username, share]) => {
+                    // Format to two decimal places if the share is a decimal
+                    const formattedShare = share % 1 === 0 ? share : share.toFixed(2);
+                    return `${username}: ${formattedShare}%`;
+                })
                 .join(', ');
         }
         return `${formattedShares}`;
@@ -20,48 +24,55 @@ function GroupExpenseList({ groupExpenses, startEditGroupExpense, deleteGroupExp
     const sortedGroupExpenses = [...groupExpenses].sort((a, b) => new Date(b.date) - new Date(a.date));
 
     return (
-        <div className="group-table-responsive" style={{    
+        <div className="scrollable-table-container" style={{    
             maxHeight: '300px', 
             overflowY: 'auto', 
-            overflowX: 'auto', // Enable horizontal scrolling
-            border: '1px solid #ccc'
+            overflowX: 'auto'
         }}>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Description</th>
-                        <th>Amount</th>
-                        <th>Paid By</th>
-                        <th>Paid For</th>
-                        <th>Date</th>
-                        <th>Split Method</th>
-                        <th>Split Details</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {sortedGroupExpenses.map((expense) => (
-                        <tr key={expense.group_expense_id}>
-                            <td>{expense.description}</td>
-                            <td>${expense.amount.toFixed(2)}</td>
-                            <td>{expense.paid_by}</td>
-                            <td className="scrollable-cell">{expense.paid_for.join(', ')}</td>
-                            <td>{new Date(expense.date).toLocaleDateString()}</td>
-                            <td>{expense.split_method}</td>
-                            <td className="scrollable-cell">{formatSplitDetails(expense.split_details)}</td>
-                            <td className='actions-column'>
-                                <button className='editExpense-button' onClick={() => startEditGroupExpense(expense.group_expense_id)}>
-                                    <EditIcon style={{color: 'black'}}/>
-                                </button>
-                                <button className='delete-button' onClick={() => deleteGroupExpense(expense.group_expense_id)}>
-                                    <DeleteIcon color='error'/>
-                                </button>
-                            </td>
+            <div className="group-table-responsive" style={{    
+                maxHeight: '300px', 
+                overflowY: 'auto', 
+                overflowX: 'auto', // Enable horizontal scrolling
+                border: '1px solid #ccc'
+            }}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Amount</th>
+                            <th>Paid By</th>
+                            <th>Paid For</th>
+                            <th>Date</th>
+                            <th>Split Method</th>
+                            <th>Split Details</th>
+                            <th>Actions</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {sortedGroupExpenses.map((expense) => (
+                            <tr key={expense.group_expense_id}>
+                                <td>{expense.description}</td>
+                                <td>${expense.amount.toFixed(2)}</td>
+                                <td>{expense.paid_by}</td>
+                                <td className="scrollable-cell">{expense.paid_for.join(', ')}</td>
+                                <td>{new Date(expense.date).toLocaleDateString()}</td>
+                                <td>{expense.split_method}</td>
+                                <td className="scrollable-cell">{formatSplitDetails(expense.split_details)}</td>
+                                <td className='actions-column'>
+                                    <button className='editExpense-button' onClick={() => startEditGroupExpense(expense.group_expense_id)}>
+                                        <EditIcon style={{color: 'black'}}/>
+                                    </button>
+                                    <button className='delete-button' onClick={() => deleteGroupExpense(expense.group_expense_id)}>
+                                        <DeleteIcon color='error'/>
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
+
     );
 }
 
